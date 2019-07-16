31c8b23801f0c18aadaebe41444970cd324a6aa0
mtrejo0 <moises.trejo0@gmail.com>
2019-07-16 14:40:06 -0700

    fixed Parse App

--- app/src/main/java/com/example/skillshop/Models/Class.java
+++ app/src/main/java/com/example/skillshop/Models/Class.java
@@ -6,7 +6,25 @@ import com.parse.ParseObject;
 @ParseClassName("Class")
 public class Class extends ParseObject {
 
-//    public final static String
+    public final static String KEY_NAME = "name";
+    public final static String KEY_DESCRIPTION = "description";
+
+    public  String getName() {
+        return getString(KEY_NAME);
+    }
+
+    public  String getDescription() {
+        return getString(KEY_DESCRIPTION);
+    }
+
+
+    public void setName(String name) {
+        put(KEY_NAME,name);
+    }
+
+    public void setDescription(String description) {
+        put(KEY_DESCRIPTION,description);
+    }
 
 
 }
