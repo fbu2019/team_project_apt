15681b7ea062f1fb3948846b0e7cff1c443d3b9a
mtrejo0 <moises.trejo0@gmail.com>
2019-07-16 11:48:01 -0700

    init

--- app/src/main/java/com/example/skillshop/MainActivity.java
+++ app/src/main/java/com/example/skillshop/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.skillshop;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // define manager to decide which fragment to display
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment fragment = new HomeFragment();
+                switch (item.getItemId()) {
+
+
+                    case R.id.home_fragment:
+                        fragment = new HomeFragment();
+                        break;
+
+                    case R.id.calendar_fragment:
+                        fragment = new CalendarFragment();
+                        break;
+
+                    case R.id.profile_fragment:
+                        fragment = new ProfileFragment();
+                        break;
+
+
+                    default: break;
+                }
+
+                // switch to selected fragment
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+
+
+            }
+        });
+
+        // default fragment in home fragment
+        bottomNavigationView.setSelectedItemId(R.id.home_fragment);
+
+
+
+
     }
 }
