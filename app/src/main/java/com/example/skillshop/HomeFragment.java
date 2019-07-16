f1008140de2a44660e8e8717ef43305ac3d2972b
Ruth-Ann Hazel Armstrong <ruthanna@fb.com>
2019-07-16 13:30:35 -0700

    recycler view dependency added

--- app/src/main/java/com/example/skillshop/HomeFragment.java
+++ app/src/main/java/com/example/skillshop/HomeFragment.java
@@ -1,8 +1,6 @@
 package com.example.skillshop;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,13 +9,12 @@ import android.view.ViewGroup;
 public class HomeFragment extends Fragment {
 
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate((R.layout.fragment_home),container,false);
     }
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
 
