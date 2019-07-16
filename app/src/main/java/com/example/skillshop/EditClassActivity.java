a2d19614988c858664214c369606d615e03833ae
Ruth-Ann Hazel Armstrong <ruthanna@fb.com>
2019-07-16 13:50:39 -0700

    empty activities and classes added

---
+++ app/src/main/java/com/example/skillshop/EditClassActivity.java
@@ -0,0 +1,13 @@
+package com.example.skillshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditClassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_class);
+    }
+}
---
+++ app/src/main/java/com/example/skillshop/Models/WorkShop.java
@@ -0,0 +1,6 @@
+package com.example.skillshop.Models;
+
+public class WorkShop {
+
+
+}
