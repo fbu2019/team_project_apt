3cb2154f13b6bbd07368001b2dbdf35c74a00ef4
Ruth-Ann Hazel Armstrong <ruthanna@fb.com>
2019-07-16 14:50:59 -0700

    recycler view connected

--- app/src/main/java/com/example/skillshop/NavigationFragments/ClassesFragment.java
+++ app/src/main/java/com/example/skillshop/NavigationFragments/ClassesFragment.java
@@ -1,26 +1,46 @@
 package com.example.skillshop.NavigationFragments;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.skillshop.ClassAdapter;
 import com.example.skillshop.R;
+
+import java.util.ArrayList;
 
 public class ClassesFragment extends Fragment {
 
+    private RecyclerView rvClasses;
+    protected ArrayList<Class> mClasses;
+    protected ClassAdapter classAdapter;
 
-    @Nullable
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         return inflater.inflate((R.layout.fragment_classes),container,false);
     }
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void onViewCreated(View view,Bundle savedInstanceState) {
+        connectRecyclerView(view);
     }
+
+    private void connectRecyclerView(View view) {
+        //find the RecyclerView
+        rvClasses = (RecyclerView) view.findViewById(R.id.rvClasses);
+        //init the arraylist (data source)
+        mClasses = new ArrayList<>();
+        //construct the adapter from this datasource
+        classAdapter = new ClassAdapter(mClasses, getContext());
+        //RecyclerView setup (layout manager, use adapter)
+        rvClasses.setLayoutManager(new LinearLayoutManager(getContext()));
+        //set the adapter
+        rvClasses.setAdapter(classAdapter);
+    }
+
 
 }
