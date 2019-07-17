3b088e2e0d46656d3cfd83512ae0a89a3bf805cd
Sabrina Pin <sbpin@fb.com>
2019-07-17 10:44:05 -0700

    Added strings for EditTexts and colors for design within values

--- app/src/main/java/com/example/skillshop/SignupActivity.java
+++ app/src/main/java/com/example/skillshop/SignupActivity.java
@@ -10,4 +10,6 @@ public class SignupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
     }
+
+    //TODO - ADD TEXTVIEWS AND A FACEBOOK LOGIN BUTTON
 }
