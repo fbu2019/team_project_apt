e6410e085a321d9048008fd720c0fc378b05a7fb
mtrejo0 <moises.trejo0@gmail.com>
2019-07-17 14:19:30 -0700

    added gallery search when added a photo the user can now add a picture of the class

--- app/src/main/java/com/example/skillshop/NewClassActivity.java
+++ app/src/main/java/com/example/skillshop/NewClassActivity.java
@@ -1,13 +1,19 @@
 package com.example.skillshop;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -25,6 +31,7 @@ public class NewClassActivity extends AppCompatActivity {
     TextView etDescription;
     TextView etCategory;
     TextView etCost;
+    ImageView ivClassImage;
 
     Button btSubmit;
 
@@ -35,12 +42,26 @@ public class NewClassActivity extends AppCompatActivity {
     String category;
     String cost;
 
+
+    // PICK_PHOTO_CODE is a constant integer
+    public final static int PICK_PHOTO_CODE = 1046;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_class);
         findAllViews();
         setSubmitListener();
+
+        ivClassImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onPickPhoto(v);
+            }
+        });
+
+
     }
 
     private void setSubmitListener() {
@@ -86,11 +107,49 @@ public class NewClassActivity extends AppCompatActivity {
         etCategory = findViewById(R.id.etCategory);
         etCost = findViewById(R.id.etCost);
         btSubmit = findViewById(R.id.btSubmit);
+        ivClassImage = findViewById(R.id.ivClassImage);
 ;
     }
 
     //TODO fix type issues
     //TODO send to parse
+
+
+
+
+    // Trigger gallery selection for a photo
+    public void onPickPhoto(View view) {
+        // Create intent for picking a photo from the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Bring up gallery to select a photo
+            startActivityForResult(intent, PICK_PHOTO_CODE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data != null) {
+            Uri photoUri = data.getData();
+            // Do something with the photo based on Uri
+
+            Bitmap selectedImage = null;
+            try {
+                selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // Load the selected image into a preview
+            ImageView ivPreview = findViewById(R.id.ivClassImage);
+            ivPreview.setImageBitmap(selectedImage);
+        }
+    }
+
 
 
 }
