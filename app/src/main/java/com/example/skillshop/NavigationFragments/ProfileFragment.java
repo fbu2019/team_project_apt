03bf417d5c2da99e1eefaf9efad109706bdfa2ca
Sabrina Pin <sbpin@fb.com>
2019-07-18 17:26:29 -0700

    User can upload a photo to ProfileFragment but it does not store locally or on databse

--- app/src/main/java/com/example/skillshop/NavigationFragments/ProfileFragment.java
+++ app/src/main/java/com/example/skillshop/NavigationFragments/ProfileFragment.java
@@ -1,10 +1,15 @@
 package com.example.skillshop.NavigationFragments;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,14 +22,18 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.skillshop.LoginActivity;
+import com.example.skillshop.NewClassActivity;
 import com.example.skillshop.R;
 import com.example.skillshop.SignupActivity;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
 import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
 import java.io.File;
+import java.io.IOException;
 
 import static java.security.AccessController.getContext;
 
@@ -34,7 +43,15 @@ public class ProfileFragment extends Fragment {
     ImageView ivProfilePic;
     EditText zipcodeInput;
     Button submitZipcodeButton;
+    Button uploadPhotoButton;
     Button logoutButton;
+
+    // PICK_PHOTO_CODE is a constant integer
+    public final static int PICK_PHOTO_CODE = 1046;
+    public final String APP_TAG = "ProfileFragment";
+    public final static int AUTOCOMPLETE_REQUEST_CODE = 42;
+    File photoFile;
+    public String photoFileName = "photo.jpg";
 
     @Nullable
     @Override
@@ -92,6 +109,15 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+        uploadPhotoButton = view.findViewById(R.id.uploadPhoto);
+        uploadPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onPickPhoto(v);
+                //  user.put("profilePicture", photoFile);
+            }
+        });
+
         logoutButton = view.findViewById(R.id.logoutButton);
         logoutButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -101,6 +127,82 @@ public class ProfileFragment extends Fragment {
                 startActivity(i);
             }
         });
+    }
+
+    // Trigger gallery selection for a photo
+    public void onPickPhoto(View view) {
+        // Create intent for picking a photo from the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        // Create a File reference to access to future access
+        // photoFile = getPhotoFileUri(photoFileName);
+
+        /*
+
+        // wrap File object into a content provider
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+        */
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Bring up gallery to select a photo
+            startActivityForResult(intent, PICK_PHOTO_CODE);
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d("ProfileFragment", "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+        return file;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if ((data != null) && (requestCode == PICK_PHOTO_CODE)){
+            Uri photoUri = data.getData();
+            // Do something with the photo based on Uri
+
+            Bitmap selectedImage = null;
+            try {
+                selectedImage = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), photoUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // Load the selected image into  preview
+            ivProfilePic.setImageBitmap(selectedImage);
+
+            //TODO: determine how to set image in database
+            /*
+            .saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if(e == null)
+                    {
+                        Toast.makeText(NewClassActivity.this, "Class was made", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                    else
+                    {
+                        Toast.makeText(NewClassActivity.this, "Class wasn't made", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+            */
+        }
 
     }
 
