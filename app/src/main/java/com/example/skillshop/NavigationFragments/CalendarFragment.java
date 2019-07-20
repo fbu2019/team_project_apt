c195701658cdf0996b5c094b4679a0cd65c02882
mtrejo0 <moises.trejo0@gmail.com>
2019-07-19 19:42:02 -0700

    calendar fragment now displays day selected

--- app/src/main/java/com/example/skillshop/NavigationFragments/CalendarFragment.java
+++ app/src/main/java/com/example/skillshop/NavigationFragments/CalendarFragment.java
@@ -5,13 +5,22 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.applandeo.materialcalendarview.CalendarView;
+import com.applandeo.materialcalendarview.EventDay;
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
 import com.example.skillshop.NewClassActivity;
 import com.example.skillshop.R;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class CalendarFragment extends Fragment {
 
@@ -21,17 +30,33 @@ public class CalendarFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate((R.layout.fragment_calendar),container,false);
     }
+
+
+    CalendarView calendarView;
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        TextView newClass = view.findViewById(R.id.nameView);
-        newClass.setOnClickListener(new View.OnClickListener() {
+
+        calendarView = view.findViewById(R.id.calendarView);
+        final List<Calendar> calendars = new ArrayList<>();
+
+        calendarView.setOnDayClickListener(new OnDayClickListener() {
+
             @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getContext(), NewClassActivity.class);
-                startActivity(i);
+            public void onDayClick(EventDay eventDay) {
+
+                Toast.makeText(getContext(), eventDay.getCalendar().getTime().toString(), Toast.LENGTH_SHORT).show();
+                calendars.add(eventDay.getCalendar());
+                calendarView.setDisabledDays(calendars);
+
+
             }
         });
+
+
+
+
+
     }
 
 }
