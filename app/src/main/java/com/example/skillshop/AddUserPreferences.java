090935b0f4f467161c988a465fe70b3d16a1c57d
Sabrina Pin <sbpin@fb.com>
2019-07-23 16:56:54 -0700

    Preferences persist across app and unchecking boxes modifies database

--- app/src/main/java/com/example/skillshop/AddUserPreferences.java
+++ app/src/main/java/com/example/skillshop/AddUserPreferences.java
@@ -32,6 +32,8 @@ public class AddUserPreferences extends AppCompatActivity {
         fitnessBox = findViewById(R.id.fitnessBox);
         artsCraftsBox = findViewById(R.id.artsCraftsBox);
 
+        checkCurrentPreferences();
+
         temporaryContinue = findViewById(R.id.continueButton);
         temporaryContinue.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,15 +44,42 @@ public class AddUserPreferences extends AppCompatActivity {
         });
     }
 
+
+    private void checkCurrentPreferences() {
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        ArrayList<String> preferences = (ArrayList<String>) currentUser.get("preferences");
+        for(int i = 0; i<preferences.size(); i++){
+            Log.e("Add user preferences", preferences.get(i)+" "+i);
+        }
+
+        for(int i = 0; i<preferences.size(); i++){
+            if(preferences.get(i).equals("Culinary")){
+                culinaryBox.setChecked(true);
+            }
+            if(preferences.get(i).equals("Education")){
+                educationBox.setChecked(true);
+            }
+            if(preferences.get(i).equals("Fitness")){
+                fitnessBox.setChecked(true);
+            }
+            if(preferences.get(i).equals("Arts and Crafts")){
+                artsCraftsBox.setChecked(true);
+            }
+        }
+
+    }
+
+
     private void retrievePreferences() {
         ArrayList <String> preferences = new ArrayList<String>();
+        //  preferences.add(0, "one");
 
         if(culinaryBox.isChecked()){
             preferences.add("Culinary");
             //currentUser.put("")
             //  add to array
         } else {
-            // TODO: ALLOW USERS TO REMOVE PREFERENCES 
+            // TODO: ALLOW USERS TO REMOVE PREFERENCES
         }
         if(educationBox.isChecked()){
             preferences.add("Education");
