657481e9d7cdc9feb50151bf1940d4762e146cab
Sabrina Pin <sbpin@fb.com>
2019-07-23 13:32:37 -0700

    Initialzie Google Pay API client

--- app/src/main/java/com/example/skillshop/PayActivity.java
+++ app/src/main/java/com/example/skillshop/PayActivity.java
@@ -6,15 +6,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.wallet.PaymentsClient;
+import com.google.android.gms.wallet.Wallet;
+import com.google.android.gms.wallet.WalletConstants;
+
 public class PayActivity extends AppCompatActivity {
 
     Button returnButton;
+    private static final int LOAD_PAYMENT_DATA_REQUEST_CODE = 42;
+    private PaymentsClient mPaymentsClient;
+    private View mGooglePayButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay);
 
+        //  Dummy button for returning to FragmentHandler
         returnButton = findViewById(R.id.returnButton);
         returnButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +31,14 @@ public class PayActivity extends AppCompatActivity {
                finish();
             }
         });
+
+        // Initialize a Google Pay API client for an environment suitable for testing
+        mPaymentsClient =
+                Wallet.getPaymentsClient(
+                        this,
+                        new Wallet.WalletOptions.Builder()
+                                .setEnvironment(WalletConstants.ENVIRONMENT_TEST)
+                                .build());
+
     }
 }
