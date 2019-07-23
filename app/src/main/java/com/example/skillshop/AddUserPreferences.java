856617a4870c89dcea0dc17e279117697d6aee0b
Sabrina Pin <sbpin@fb.com>
2019-07-23 16:46:09 -0700

    Users can add preferences via AddUserPreferences. Database updates

--- app/src/main/java/com/example/skillshop/AddUserPreferences.java
+++ app/src/main/java/com/example/skillshop/AddUserPreferences.java
@@ -2,10 +2,17 @@ package com.example.skillshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Checkable;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.ArrayList;
 
 public class AddUserPreferences extends AppCompatActivity {
 
@@ -29,7 +36,45 @@ public class AddUserPreferences extends AppCompatActivity {
         temporaryContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                retrievePreferences();
                 finish();
+            }
+        });
+    }
+
+    private void retrievePreferences() {
+        ArrayList <String> preferences = new ArrayList<String>();
+
+        if(culinaryBox.isChecked()){
+            preferences.add("Culinary");
+            //currentUser.put("")
+            //  add to array
+        } else {
+            // TODO: ALLOW USERS TO REMOVE PREFERENCES 
+        }
+        if(educationBox.isChecked()){
+            preferences.add("Education");
+            //  add to array
+        }
+        if(fitnessBox.isChecked()){
+            preferences.add("Fitness");
+            //  add to array
+        }
+        if(artsCraftsBox.isChecked()){
+            preferences.add("Arts and Crafts");
+            //  add to array
+        }
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        currentUser.put("preferences", preferences);
+
+        currentUser.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    e.printStackTrace();
+                    return;
+                }
             }
         });
     }
