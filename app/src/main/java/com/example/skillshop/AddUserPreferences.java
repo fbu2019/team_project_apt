e303f4d903ed20a7bac178ff7dfaccf364eb643d
Sabrina Pin <sbpin@fb.com>
2019-07-23 16:33:41 -0700

    Added CheckBoxes and display message in AddUserPreferences

--- app/src/main/java/com/example/skillshop/AddUserPreferences.java
+++ app/src/main/java/com/example/skillshop/AddUserPreferences.java
@@ -4,15 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Checkable;
 
 public class AddUserPreferences extends AppCompatActivity {
 
     Button temporaryContinue;
+    CheckBox culinaryBox;
+    CheckBox educationBox;
+    CheckBox fitnessBox;
+    CheckBox artsCraftsBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user_preferences);
+
+        culinaryBox = findViewById(R.id.culinaryBox);
+        educationBox = findViewById(R.id.educationBox);
+        fitnessBox = findViewById(R.id.fitnessBox);
+        artsCraftsBox = findViewById(R.id.artsCraftsBox);
 
         temporaryContinue = findViewById(R.id.continueButton);
         temporaryContinue.setOnClickListener(new View.OnClickListener() {
