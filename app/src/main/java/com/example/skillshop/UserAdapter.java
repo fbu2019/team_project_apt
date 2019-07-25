bb3971ac726cac77dada47bd9b32ebfc9d8c1198
Ruth-Ann Hazel Armstrong <ruthanna@fb.com>
2019-07-24 17:08:26 -0700

    user adapter partially implemented

---
+++ app/src/main/java/com/example/skillshop/UserAdapter.java
@@ -0,0 +1,34 @@
+package com.example.skillshop;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import com.example.skillshop.Models.Workshop;
+
+import java.util.List;
+
+public class UserAdapter{ /* extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+    private static List<Workshop> mWorkshops;
+    private Context context;
+    @Override
+    public UserAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserAdapter.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+    }*/
+
+}
