5e48cee7f6f72dfaebc22c4c8aad26e73f1f0529
mtrejo0 <moises.trejo0@gmail.com>
2019-07-26 11:03:14 -0700

    users can now add messages to ur db

--- app/src/main/java/com/example/skillshop/ChatActivity.java
+++ app/src/main/java/com/example/skillshop/ChatActivity.java
@@ -2,12 +2,61 @@ package com.example.skillshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class ChatActivity extends AppCompatActivity {
+
+    EditText etMessage;
+    Button btSend;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        setupMessagePosting();
     }
+
+
+
+    // Setup button event handler which posts the entered message to Parse
+    void setupMessagePosting() {
+        // Find the text field and button
+        etMessage = (EditText) findViewById(R.id.etMessage);
+        btSend = (Button) findViewById(R.id.btSend);
+        // When send button is clicked, create message object on Parse
+        btSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = etMessage.getText().toString();
+                ParseObject message = ParseObject.create("Message");
+                message.put("userId", ParseUser.getCurrentUser().getObjectId());
+                message.put("body", data);
+                message.put("name",ParseUser.getCurrentUser().get("firstName"));
+                message.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e == null) {
+                            Toast.makeText(ChatActivity.this, "Successfully created message on Parse",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.e("ChatActivity", "Failed to save message", e);
+                        }
+                    }
+
+                });
+                etMessage.setText(null);
+            }
+        });
+    }
+
 }
