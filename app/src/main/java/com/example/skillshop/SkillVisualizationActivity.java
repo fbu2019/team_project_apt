da5f0bd4e4556db1dd6b69482c737bcc36707c4e
Ruth-Ann Hazel Armstrong <ruthanna@fb.com>
2019-07-30 14:36:27 -0700

    skill visualizational tool works for classes the user has signed up for

--- app/src/main/java/com/example/skillshop/SkillVisualizationActivity.java
+++ app/src/main/java/com/example/skillshop/SkillVisualizationActivity.java
@@ -22,6 +22,7 @@ import com.parse.FindCallback;
 import com.parse.ParseException;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -34,83 +35,122 @@ public class SkillVisualizationActivity extends AppCompatActivity {
 
         AnyChartView anyChartView = findViewById(R.id.any_chart_view);
         anyChartView.setProgressBar(findViewById(R.id.progress_bar));
-
+        final Integer[][] takingCategories = new Integer[1][5];
+        final Integer[][] teachingCategories = new Integer[1][5];
         Polar polar = AnyChart.polar();
 
         List<DataEntry> data = new ArrayList<>();
 
-  //
+        final int[] check = {0};
+        Query parseQueryTaking = new Query();
+        parseQueryTaking.getClassesTaking();
 
-        String[] categoryArray= {"Culinary", "Education", "Fitness", "Arts/Crafts", "Other"};
-        for (String category:categoryArray){
-            final int[] teachTake = new int[2];
-            Query parseQuery = new Query();
-            ArrayList<String> singletonCategory = new ArrayList<String>();
-            singletonCategory.add("Culinary");
-            parseQuery.getClassesTaking().byCategory(singletonCategory);
-            parseQuery.findInBackground(new FindCallback<Workshop>() {
-                @Override
-                public void done(List<Workshop> objects, ParseException e) {
-                    if (e == null) {
-                        teachTake[0] = objects.size();
-                    } else {
-                        e.printStackTrace();
-                    }
-                }
-            });
+        parseQueryTaking.findInBackground(new FindCallback<Workshop>() {
+            @Override
+            public void done(List<Workshop> takingWorkshops, ParseException e) {
+                if (e == null) {
+                   takingCategories[0] = countCategories(takingWorkshops);
 
-            parseQuery.getClassesTeaching().byCategory(singletonCategory);
-            parseQuery.findInBackground(new FindCallback<Workshop>() {
-                @Override
-                public void done(List<Workshop> objects, ParseException e) {
-                    if (e == null) {
-                        teachTake[1] = objects.size();
-                    } else {
-                        e.printStackTrace();
-                    }
+
+                    Query parseQueryTeaching = new Query();
+                    parseQueryTeaching.getClassesTeaching();
+
+                    parseQueryTeaching.findInBackground(new FindCallback<Workshop>() {
+                        @Override
+                        public void done(List<Workshop> teachingWorkshops, ParseException e) {
+                            if (e == null) {
+                                teachingCategories[0] = countCategories(teachingWorkshops);
+
+                                data.add(new CustomDataEntry("Culinary", teachingCategories[0][0], takingCategories[0][0]));
+                                data.add(new CustomDataEntry("Education", teachingCategories[0][1], takingCategories[0][1]));
+                                data.add(new CustomDataEntry("Fitness", teachingCategories[0][2], takingCategories[0][2]));
+                                data.add(new CustomDataEntry("Arts/Crafts", teachingCategories[0][3], takingCategories[0][3]));
+                                data.add(new CustomDataEntry("Other", teachingCategories[0][4], takingCategories[0][4]));
+                                Set set = Set.instantiate();
+                                set.data(data);
+                                Mapping series1Data = set.mapAs("{ x: 'x', value: 'value' }");
+                                Mapping series2Data = set.mapAs("{ x: 'x', value: 'value2' }");
+
+                                polar.column(series1Data);
+
+                                polar.column(series2Data);
+
+
+                                polar.title("Skill Graph");
+
+                                polar.sortPointsByX(true)
+                                        .defaultSeriesType(PolarSeriesType.COLUMN)
+                                        .yAxis(false)
+                                        .xScale(ScaleTypes.ORDINAL);
+
+                                polar.title().margin().bottom(20d);
+
+                                ((Linear) polar.yScale(Linear.class)).stackMode(ScaleStackMode.VALUE);
+
+                                polar.tooltip()
+                                        .displayMode(TooltipDisplayMode.UNION);
+
+                                anyChartView.setChart(polar);
+
+                            } else {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+
+
+
+                } else {
+                    e.printStackTrace();
                 }
-            });
-            data.add(new CustomDataEntry(singletonCategory.get(0), 12814, 4376));
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    private Integer[]  countCategories(List<Workshop> workshops) {
+        Integer[] categoryCount = Collections.nCopies(5, 0).toArray(new Integer[0]);
+        for (int i = 0; i < workshops.size(); i++){
+            if (workshops.get(i).getCategory().equals("Culinary")){
+                categoryCount[0]++;
+
+            }
+            if (workshops.get(i).getCategory().equals("Education")){
+                categoryCount[1]++;
+
+            }
+            if (workshops.get(i).getCategory().equals("Fitness")){
+                categoryCount[2]++;
+
+
+            }
+            if (workshops.get(i).getCategory().equals("Arts/Crafts")){
+                categoryCount[3]++;
+
+            }
+            if (workshops.get(i).getCategory().equals("Other")){
+                categoryCount[4]++;
+            }
         }
-
-
-    //
-        data.add(new CustomDataEntry("Culinary", 12814, 4376));
-        data.add(new CustomDataEntry("Education", 13012, 3987));
-        data.add(new CustomDataEntry("Fitness", 11624, 3574 ));
-        data.add(new CustomDataEntry("Arts/Crafts", 8814, 4376));
-        data.add(new CustomDataEntry("Other", 12998, 4572 ));
+       return categoryCount;
 
 
 
+    }
 
+    private void countTeachingCategories() {
 
-//
-        Set set = Set.instantiate();
-        set.data(data);
-        Mapping series1Data = set.mapAs("{ x: 'x', value: 'value' }");
-        Mapping series2Data = set.mapAs("{ x: 'x', value: 'value2' }");
-
-        polar.column(series1Data);
-
-        polar.column(series2Data);
-
-
-        polar.title("Skill Graph");
-
-        polar.sortPointsByX(true)
-                .defaultSeriesType(PolarSeriesType.COLUMN)
-                .yAxis(false)
-                .xScale(ScaleTypes.ORDINAL);
-
-        polar.title().margin().bottom(20d);
-
-        ((Linear) polar.yScale(Linear.class)).stackMode(ScaleStackMode.VALUE);
-
-        polar.tooltip()
-                .displayMode(TooltipDisplayMode.UNION);
-
-        anyChartView.setChart(polar);
     }
 
     private class CustomDataEntry extends ValueDataEntry {
