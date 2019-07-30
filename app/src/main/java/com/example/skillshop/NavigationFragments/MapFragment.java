f784a155ee5e78e21a606b443a6ff7563d8c28b5
mtrejo0 <moises.trejo0@gmail.com>
2019-07-29 17:31:25 -0700

    added maps constants

--- app/src/main/java/com/example/skillshop/NavigationFragments/MapFragment.java
+++ app/src/main/java/com/example/skillshop/NavigationFragments/MapFragment.java
@@ -1,5 +1,6 @@
 package com.example.skillshop.NavigationFragments;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,9 +9,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.skillshop.Models.Workshop;
 import com.example.skillshop.R;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import java.util.ArrayList;
 
 public class MapFragment extends Fragment {
+
+    private SupportMapFragment mapFragment;
+    private GoogleMap map;
+    private LocationRequest mLocationRequest;
+    Location mCurrentLocation;
+    private long UPDATE_INTERVAL = 60000;  /* 60 secs */
+    private long FASTEST_INTERVAL = 5000; /* 5 secs */
+    protected ArrayList<Workshop> mWorkshops;
+
+    private final static String KEY_LOCATION = "location";
+
+    /*
+     * Define a request code to send to Google Play services This code is
+     * returned in Activity.onActivityResult
+     */
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 
     @Nullable
     @Override
