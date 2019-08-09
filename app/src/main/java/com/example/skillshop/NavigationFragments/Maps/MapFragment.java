1ff401fe2b0e8dffea12176172b2359989758d1b
Ruth-Ann Hazel Armstrong <ruthanna@fb.com>
2019-08-08 17:37:11 -0700

    code for map fragment organized and decomposed

--- app/src/main/java/com/example/skillshop/NavigationFragments/Maps/MapFragment.java
+++ app/src/main/java/com/example/skillshop/NavigationFragments/Maps/MapFragment.java
@@ -42,49 +42,48 @@ public class MapFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_maps, container, false);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);  //use SupportMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @SuppressLint("MissingPermission")
             @Override
             public void onMapReady(GoogleMap mMap) {
                 mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
-              //  mMap.setMyLocationEnabled(true);
                 mMap.clear(); //clear old markers
-
-
-                CameraPosition googlePlex = CameraPosition.builder()
-                        .target(new LatLng(37.4530,-122.1817))
-                        .zoom(10)
-
-                        .bearing(0)
-                        .tilt(45)
-                        .build();
-
-                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 2000, null);
-                mMap.setInfoWindowAdapter(new CustomWindowAdapter(getLayoutInflater(), getContext()));
-                mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
-                    @Override
-                    public void onInfoWindowClick(Marker marker) {
-                        Workshop workshop = (Workshop) marker.getTag();
-                        final Intent profileDetailsIntent = new Intent(getContext(), ClassDetailsActivity.class);
-                        //pass in class that was selected
-                        profileDetailsIntent.putExtra(Workshop.class.getSimpleName(), Parcels.wrap(workshop));
-                        startActivity(profileDetailsIntent);
-
-                    }
-                });
-                loadMap(mMap);
-
+                setupCameraPosition();
+                setupInfoWindow();
+                getClasses(mMap);
             }
         });
-
-
         return rootView;
+    }
 
+    private void setupInfoWindow() {
+        mMap.setInfoWindowAdapter(new CustomWindowAdapter(getLayoutInflater(), getContext()));
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Workshop workshop = (Workshop) marker.getTag();
+                final Intent profileDetailsIntent = new Intent(getContext(), ClassDetailsActivity.class);
+                //pass in class that was selected
+                profileDetailsIntent.putExtra(Workshop.class.getSimpleName(), Parcels.wrap(workshop));
+                startActivity(profileDetailsIntent);
+            }
+        });
+    }
+
+    private void setupCameraPosition() {
+        CameraPosition googlePlex = CameraPosition.builder()
+                .target(new LatLng(37.4530,-122.1817))
+                .zoom(10)
+                .bearing(0)
+                .tilt(45)
+                .build();
+
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 2000, null);
     }
 
 
@@ -94,7 +93,7 @@ public class MapFragment extends Fragment{
 
     }
 
-    public void loadMap(GoogleMap mMap)
+    public void getClasses(GoogleMap mMap)
     {
         mWorkshops = new ArrayList<>();
         Query parseQuery = new Query();
@@ -109,28 +108,27 @@ public class MapFragment extends Fragment{
                         Workshop workshopItem = objects.get(i);
                         mWorkshops.add(workshopItem);
 
-                        // Define color of marker icon
+                        //Creates point from workshop location to be used for the position of the marker
                         ParseGeoPoint workshopLocation = workshopItem.getLocation();
                         double lat = workshopLocation.getLatitude();
                         double lon = workshopLocation.getLongitude();
                         LatLng point = new LatLng(lat, lon);
-                        String category = workshopItem.getCategory();
+
 
                         //sets the default marker color
                         BitmapDescriptor defaultMarker = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA);
                         //sets the marker color based on category
+                        String category = workshopItem.getCategory();
                         defaultMarker = setMarkerColor(defaultMarker, category);
 
                         Marker marker = mMap.addMarker(new MarkerOptions()
                                 .position(point)
                                 .icon(defaultMarker)
                                 .title(workshopItem.getObjectId())
-                               // .snippet(getRelativeTimeAgo(workshopItem.getDate()))
                         );
 
+                        //adds each workshop item as a tag to be passed to the class details activity when the info window is pressed
                         marker.setTag(workshopItem);
-
-
                     }
                 } else {
                     e.printStackTrace();
